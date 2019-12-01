73c34f1f8ed4b56588a35903080650ec68fcbb80
Casey M <caseym@gmail.com>
2019-11-30 21:06:37 -0600

    catch invalid symlink URLs

--- step.py
+++ step.py
@@ -916,6 +916,8 @@ class GithubCitationFileStep(CitationFileStep):
         repo_path = matches[0].replace("/blob/master/CITATION", "")
         api_url = 'https://api.github.com/repos{}/contents/CITATION?ref=master'.format(repo_path)
         r = requests.get(api_url)
+        if r.status_code is not 200:
+            return None
         api_resp = r.json()
         encoded_content = api_resp.get('content')
         decoded_content = base64.b64decode(encoded_content).decode('utf-8')
