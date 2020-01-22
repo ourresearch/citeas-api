01c57cea812f65ee122a5380eb351adc47d804bf
Casey M <caseym@gmail.com>
2020-01-22 08:35:03 -0600

    refactor get_subject function, delete unnecessary files

--- install_emacs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x #echo on
-
-curl https://s3-us-west-2.amazonaws.com/heroku-emacs/emacs-24.5-heroku-minimal.tar --location --silent | tar x
-PATH=$PATH:/app/emacs/bin
-export PATH
-
