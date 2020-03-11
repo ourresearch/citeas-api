9470d68704f2116db53c632bd5122f063a3e43b7
Casey M <caseym@gmail.com>
2020-03-10 21:20:32 -0500

    update tests

--- test/test_software.py
+++ test/test_software.py
@@ -41,7 +41,7 @@ test_urls = [
     ("https://github.com/nicholasricci/DDM_Framework", "", "Marzolla, M., D'Angelo, G. & Mandrioli, M., 2013. A Parallel Data Distribution Management Algorithm."),
     ("https://cran.r-project.org/web/packages/vistime/index.html", "", "Raabe, S., 2020. vistime: Pretty Timeline Creation. <i>R package version 0.9.0</i>. Available at: https://CRAN.R-project.org/package=vistime."),
     ("https://cran.r-project.org/web/packages/afCEC/index.html", "", "Byrski, K., 2018. afCEC: Active Function Cross-Entropy Clustering. <i>R package version 1.0.2</i>. Available at: https://CRAN.R-project.org/package=afCEC."),
-    ("http://cran.r-project.org/package=arcos", "", "Steven, R., Andrew, B. & Aaron, W., 2019. arcos: Load ARCOS Prescription Data Prepared by the Washington Post. <i>R package version 0.8.2</i>. Available at: https://CRAN.R-project.org/package=arcos."),
+    ("http://cran.r-project.org/package=arcos", "", "Steven, R., Andrew, B. & Aaron, W., 2020. arcos: Load ARCOS Prescription Data Prepared by the Washington Post. <i>R package version 1.0</i>. Available at: https://CRAN.R-project.org/package=arcos."),
     ("https://ccdproc.readthedocs.io/en/latest/", "", "Craig, M. et al., 2017. Astropy/Ccdproc: V1.3.0.Post1. Available at: https://doi.org/10.5281/zenodo.1069648."),
     ("https://photutils.readthedocs.io/", "", "Bradley, L. et al., 2019. astropy/photutils: v0.7.2. Available at: https://doi.org/10.5281/zenodo.596036.")
 ]
