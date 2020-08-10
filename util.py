596acac94be25c3e358e081193fc8089724b720d
Casey M <caseym@gmail.com>
2020-08-10 16:55:28 -0500

    remove unused utility functions

--- util.py
+++ util.py
@@ -1,149 +1,20 @@
-import bisect
-import collections
-import datetime
-import logging
-import math
-import os
 import re
-import time
 import unicodedata
 import urllib.parse
 from html import escape
 
 import requests
 from nameparser import HumanName
-from unidecode import unidecode
 
 
 class NoDoiException(Exception):
     pass
 
 
-# from http://stackoverflow.com/a/3233356/596939
-def update_recursive_sum(d, u):
-    for k, v in u.items():
-        if isinstance(v, collections.Mapping):
-            r = update_recursive_sum(d.get(k, {}), v)
-            d[k] = r
-        else:
-            if k in d:
-                d[k] += u[k]
-            else:
-                d[k] = u[k]
-    return d
-
-
-# returns dict with values that are proportion of all values
-def as_proportion(my_dict):
-    if not my_dict:
-        return {}
-    total = sum(my_dict.values())
-    resp = {}
-    for k, v in my_dict.items():
-        resp[k] = round(float(v)/total, 2)
-    return resp
-
-
-def calculate_percentile(refset, value):
-    if value is None:  # distinguish between that and zero
-        return None
-
-    matching_index = bisect.bisect_left(refset, value)
-    percentile = float(matching_index) / len(refset)
-    # print u"percentile for {} is {}".format(value, percentile)
-
-    return percentile
-
-
 def clean_html(raw_html):
-  cleanr = re.compile('<.*?>')
-  cleantext = re.sub(cleanr, '', raw_html)
-  return cleantext
-
-
-# good for deduping strings.  warning: output removes spaces so isn't readable.
-def normalize(text):
-    response = text.lower()
-    response = unidecode(str(response))
-    response = clean_html(response)  # has to be before remove_punctuation
-    response = remove_punctuation(response)
-    response = re.sub("\s+", "", response)
-    for stop_word in ["a", "an", "the"]:
-        response = response.replace(stop_word, "")
-    return response
-
-
-def remove_punctuation(input_string):
-    # from http://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
-    no_punc = input_string
-    if input_string:
-        no_punc = "".join(e for e in input_string if (e.isalnum() or e.isspace()))
-    return no_punc
-
-
-# from http://stackoverflow.com/a/11066579/596939
-def replace_punctuation(text, sub):
-    punctutation_cats = set(['Pc', 'Pd', 'Ps', 'Pe', 'Pi', 'Pf', 'Po'])
-    chars = []
-    for my_char in text:
-        if unicodedata.category(my_char) in punctutation_cats:
-            chars.append(sub)
-        else:
-            chars.append(my_char)
-    return "".join(chars)
-
-
-# from http://stackoverflow.com/a/22238613/596939
-def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
-
-    if isinstance(obj, datetime):
-        serial = obj.isoformat()
-        return serial
-    raise TypeError ("Type not serializable")
-
-
-def conversational_number(number):
-    words = {
-        "1.0": "one",
-        "2.0": "two",
-        "3.0": "three",
-        "4.0": "four",
-        "5.0": "five",
-        "6.0": "six",
-        "7.0": "seven",
-        "8.0": "eight",
-        "9.0": "nine",
-    }
-
-    if number < 1:
-        return round(number, 2)
-
-    elif number < 1000:
-        return int(math.floor(number))
-
-    elif number < 1000000:
-        divided = number / 1000.0
-        unit = "thousand"
-
-    else:
-        divided = number / 1000000.0
-        unit = "million"
-
-    short_number = '{}'.format(round(divided, 2))[:-1]
-    if short_number in words:
-        short_number = words[short_number]
-
-    return short_number + " " + unit
-
-
-def is_doi_url(url):
-    # test urls at https://regex101.com/r/yX5cK0/2
-    p = re.compile("https?:\/\/(?:dx.)?doi.org\/(.*)")
-    matches = re.findall(p, url)
-    if len(matches) > 0:
-        return True
-    return False
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
 
 
 def clean_doi(dirty_doi, code_meta_exists=False):
@@ -176,143 +47,7 @@ def clean_doi(dirty_doi, code_meta_exists=False):
     return resp
 
 
-def pick_best_url(urls):
-    if not urls:
-        return None
-
-    #get a backup
-    response = urls[0]
-
-    # now go through and pick the best one
-    for url in urls:
-        # doi if available
-        if "doi.org" in url:
-            response = url
-
-        # anything else if what we currently have is bogus
-        if response == "http://www.ncbi.nlm.nih.gov/pmc/articles/PMC":
-            response = url
-
-    return response
-
-
-def date_as_iso_utc(datetime_object):
-    if datetime_object is None:
-        return None
-
-    date_string = "{}{}".format(datetime_object, "+00:00")
-    return date_string
-
-
-def dict_from_dir(obj, keys_to_ignore=None, keys_to_show="all"):
-
-    if keys_to_ignore is None:
-        keys_to_ignore = []
-    elif isinstance(keys_to_ignore, str):
-        keys_to_ignore = [keys_to_ignore]
-
-    ret = {}
-
-    if keys_to_show != "all":
-        for key in keys_to_show:
-            ret[key] = getattr(obj, key)
-
-        return ret
-
-    for k in dir(obj):
-        value = getattr(obj, k)
-
-        if k.startswith("_"):
-            pass
-        elif k in keys_to_ignore:
-            pass
-        # hide sqlalchemy stuff
-        elif k in ["query", "query_class", "metadata"]:
-            pass
-        elif callable(value):
-            pass
-        else:
-            try:
-                # convert datetime objects...generally this will fail becase
-                # most things aren't datetime object.
-                ret[k] = time.mktime(value.timetuple())
-            except AttributeError:
-                ret[k] = value
-    return ret
-
-
-def median(my_list):
-    """
-    Find the median of a list of ints
-
-    from https://stackoverflow.com/questions/24101524/finding-median-of-list-in-python/24101655#comment37177662_24101655
-    """
-    my_list = sorted(my_list)
-    if len(my_list) < 1:
-            return None
-    if len(my_list) %2 == 1:
-            return my_list[((len(my_list)+1)/2)-1]
-    if len(my_list) %2 == 0:
-            return float(sum(my_list[(len(my_list)/2)-1:(len(my_list)/2)+1]))/2.0
-
-
-def underscore_to_camelcase(value):
-    words = value.split("_")
-    capitalized_words = []
-    for word in words:
-        capitalized_words.append(word.capitalize())
-
-    return "".join(capitalized_words)
-
-
-def chunks(l, n):
-    """
-    Yield successive n-sized chunks from l.
-
-    from http://stackoverflow.com/a/312464
-    """
-    for i in range(0, len(l), n):
-        yield l[i:i+n]
-
-
-def page_query(q, page_size=1000):
-    offset = 0
-    while True:
-        r = False
-        print("util.page_query() retrieved {} things".format(page_query()))
-        for elem in q.limit(page_size).offset(offset):
-            r = True
-            yield elem
-        offset += page_size
-        if not r:
-            break
-
-
-def elapsed(since, round_places=2):
-    return round(time.time() - since, round_places)
-
-
-def truncate(str, max=100):
-    if len(str) > max:
-        return str[0:max] + "..."
-    else:
-        return str
-
-
-def str_to_bool(x):
-    if x.lower() in ["true", "1", "yes"]:
-        return True
-    elif x.lower() in ["false", "0", "no"]:
-        return False
-    else:
-        raise ValueError("This string can't be cast to a boolean.")
-
-
-# from http://stackoverflow.com/a/20007730/226013
-ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
-
-
-#from http://farmdev.com/talks/unicode/
+# from http://farmdev.com/talks/unicode/
 def to_unicode_or_bust(obj, encoding='utf-8'):
     if isinstance(obj, str):
         if not isinstance(obj, str):
@@ -337,42 +72,6 @@ def remove_nonprinting_characters(input, encoding='utf-8'):
         response = response.encode(encoding)
 
     return response
-
-
-# getting a "decoding Unicode is not supported" error in this function?
-# might need to reinstall libaries as per
-# http://stackoverflow.com/questions/17092849/flask-login-typeerror-decoding-unicode-is-not-supported
-class HTTPMethodOverrideMiddleware(object):
-    allowed_methods = frozenset([
-        'GET',
-        'HEAD',
-        'POST',
-        'DELETE',
-        'PUT',
-        'PATCH',
-        'OPTIONS'
-    ])
-    bodyless_methods = frozenset(['GET', 'HEAD', 'OPTIONS', 'DELETE'])
-
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        method = environ.get('HTTP_X_HTTP_METHOD_OVERRIDE', '').upper()
-        if method in self.allowed_methods:
-            method = method.encode('ascii', 'replace')
-            environ['REQUEST_METHOD'] = method
-        if method in self.bodyless_methods:
-            environ['CONTENT_LENGTH'] = '0'
-        return self.app(environ, start_response)
-
-
-def get_random_dois(n):
-    url = "http://api.crossref.org/works?filter=from-pub-date:2006-01-01&sample={}".format(n)
-    r = requests.get(url)
-    items = r.json()["message"]["items"]
-    dois = [item["DOI"] for item in items]
-    print(dois)
 
 
 def get_raw_bitbucket_url(url):
@@ -467,7 +166,6 @@ def get_webpage_text(starting_url):
         url = hops[0]
         r = requests.get(url)
     except Exception:
-        # print u"exception getting the webpage {}".format(url)
         return
     return r.text
 
@@ -520,14 +218,13 @@ def get_bibtex_url(text):
 
 
 def extract_bibtex(text):
-    valid_entry_types = ['article', 'book', 'booklet', 'conference', 'inbook', 'incollection', \
-                        'inproceedings', 'manual', 'mastersthesis', 'misc', 'phdthesis', 'proceedings', \
+    valid_entry_types = ['article', 'book', 'booklet', 'conference', 'inbook', 'incollection',
+                        'inproceedings', 'manual', 'mastersthesis', 'misc', 'phdthesis', 'proceedings',
                          'techreport', 'unpublished']
     if not text:
         return None
     try:
         entry_type = re.findall(r"(@\w+-?\w+)", text, re.MULTILINE | re.DOTALL)[0]
-        myvar = entry_type[1:]
         if entry_type[1:] not in valid_entry_types:
             return None
         result = re.findall(r"@\w+-?\w+{.*}", text, re.MULTILINE | re.DOTALL)[0]
